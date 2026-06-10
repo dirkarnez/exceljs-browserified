012c1236548a5efa29680e13106f499ad75dd1c3
Dirk Arnez <smalldirkalex@gmail.com>
2026-06-10 17:00:14 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// import { sum } from "./calculator";
+
+// console.log(`1 + 10 = ${sum(1, 10)}`)
+// // import PDFDocument from 'pdfkit';
+// // import PDFDocument from "pdfkit/js/";
+// // export default PDFDocument;
+
+
+
+// ts-ignore
+// export { default } from 'fastest-levenshtein';
+export * from 'fastest-levenshtein';
