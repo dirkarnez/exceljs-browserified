352c6f822ae626f0cf8ac4c66fa3466130e8ed70
Dirk Arnez <smalldirkalex@gmail.com>
2026-06-10 17:00:53 +0800

    - update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,12 @@
 
 // ts-ignore
 // export { default } from 'fastest-levenshtein';
-export * from 'fastest-levenshtein';
+// export * from 'fastest-levenshtein';
+
+import exceljs from "exceljs";
+import { ReadableWebToNodeStream } from "readable-web-to-node-stream";
+
+export const readCSV = async (file: File) => {
+  // The stream() method returns a native Web ReadableStream
+  return await new exceljs.Workbook().csv.read(new ReadableWebToNodeStream(file.stream()));
+}
